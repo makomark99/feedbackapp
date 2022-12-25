a8022310f1d777a6e4b7e2353a0bf50c66a3b69b
Makó Márk <makomark99@gmail.com>
2022-12-25 22:14:00 +0100

    2022.12.25

--- includes/datafromserver.inc.php
+++ includes/datafromserver.inc.php
@@ -1,28 +1,13 @@
 <?php
 require_once 'databasehandler.inc.php';
-$sql1="SELECT count(*) as 'Nem elégedett' from answers Where SATISFACTION = '1'";
-$sql2="SELECT count(*) as 'Átlagos' from answers Where SATISFACTION = '2'";
-$sql3="SELECT count(*) as 'Elégedett' from answers Where SATISFACTION = '3'";
-$sql4="SELECT count(*) as 'Nagyon elégedett' from answers Where SATISFACTION = '4'";
-$result1 = mysqli_query($conn, $sql1);
-$result2 = mysqli_query($conn, $sql2);
-$result3 = mysqli_query($conn, $sql3);
-$result4 = mysqli_query($conn, $sql4);
-$row1 = mysqli_fetch_array($result1);
-$row2 = mysqli_fetch_array($result2);
-$row3 = mysqli_fetch_array($result3);
-$row4 = mysqli_fetch_array($result4);
+  $sql="SELECT SATISFACTION, COUNT(ID) as rating_count FROM answers WHERE SATISFACTION != 0 AND SATISFACTION >= 1 AND SATISFACTION <= 4 GROUP BY SATISFACTION UNION ALL SELECT SATISFACTION, 0 as rating_count FROM (SELECT 1 as SATISFACTION UNION ALL SELECT 2 as SATISFACTION UNION ALL SELECT 3 as SATISFACTION UNION ALL SELECT 4 as SATISFACTION) t WHERE NOT EXISTS (SELECT 1 FROM answers r WHERE SATISFACTION = t.SATISFACTION) ORDER BY SATISFACTION DESC;";
+  $result = mysqli_query($conn, $sql);
+  $array_with_ratings=array();
+  while ($row=mysqli_fetch_assoc($result)) {
+      $array_with_ratings[] = $row['rating_count'];
+  }
+    ?>
 
-$data = array(
-    "nem_elegedett" => $row1['Nem elégedett'],
-    "atlagos" => $row2['Átlagos'],
-    "elegedett" => $row3['Elégedett'],
-    "nagyon_elegedett" => $row4['Nagyon elégedett']
-  );
-  $json_data = json_encode($data);
-  file_put_contents('data.json', $json_data);
-  $json_data = file_get_contents('data.json');
-  $data = json_decode($json_data, true);
-  
-
-?>
+<script>
+  const data_array = <?php echo json_encode($array_with_ratings);?>
+</script>
--- result.php
+++ result.php
@@ -1,61 +1,92 @@
-<?php 
+<?php
  require_once 'includes/header.php';
  require_once 'includes/navbar.php';
  require_once 'includes/databasehandler.inc.php';
  require 'includes/datafromserver.inc.php';
- if(!isset($_SESSION['id'])){
-    header('Location: ./index.php');
-    exit();
+ if (!isset($_SESSION['id'])) {
+     header('Location: ./index.php');
+     exit();
  }
  
 
  ?>
- <body>
+
+<body>
     <div class="container">
         <div class="row">
-        <div class="card mt-5 mx-auto" style="width: 50rem;">
-            <div class="card-body">
-                <h1 class="card-title text-center mb-5 mt-3"> <strong>Kimutatások</strong></h1>
-                    <canvas id="my_Chart" width="200" height="100"></canvas>
+            <div class="card mt-5 mx-auto" style="width: 50rem;">
+                <div class="card-body">
+                    <h1 class="card-title text-center mb-5 mt-3"> <strong>Kimutatások</strong></h1>
+                    <canvas id="my_Chart" height="200px"></canvas>
                     <script>
-                        const ctx = document.getElementById('my_Chart');
-                        fetch('data.json')
-                        .then(response => response.json())
-                        .then(data => {
-                            var xValues = ['Nem elegedett', "Átlagos", "Elégedett", "Nagyon elégedett"];
-                            var yValues = [data.nem_elegedett, data.atlagos, data.elegedett, data.nagyon_elegedett];
-                            var barColors = [
-                            "red",
-                            "yellow",
-                            "lightgreen",
-                            "#14C900",
-                            ];
+                        window.onload = function() {
+                            const ctx = document.getElementById('my_Chart');
                             new Chart(ctx, {
-                                type: "bar",
+                                type: 'bar',
                                 data: {
-                                    labels: xValues,
+                                    labels: ['Elégedetlen', 'Átlagos', 'Elégedett', 'Nagyon elégedett']
+                                        .reverse(),
                                     datasets: [{
-                                    backgroundColor: barColors,
-                                    data: yValues
+                                        label: 'Vevői visszajelzés',
+                                        data: data_array,
+                                        borderWidth: 1,
+                                        backgroundColor: '#0758a4',
+
                                     }]
                                 },
                                 options: {
-                                    title: {
-                                    display: true,
-                                    text: "Elégedettség"
-                                    }
+                                    responsive: true,
+                                    plugins: {
+                                        legend: {
+                                            labels: {
+                                                // This more specific font property overrides the global property
+                                                font: {
+                                                    size: 16,
+                                                    family: 'Nunito',
+                                                },
+                                            }
+                                        },
+                                        title: {
+                                            display: true,
+                                            text: 'Vevői elégedettségek (1-4)',
+                                            font: {
+                                                size: 24,
+                                                family: 'Nunito'
+                                            },
+                                            color: 'black',
+                                        },
+                                    },
+                                    scales: {
+                                        y: {
+                                            beginAtZero: true,
+                                            ticks: {
+                                                font: {
+                                                    size: 18,
+                                                    family: 'Nunito',
+
+                                                },
+                                                color: 'black'
+                                            },
+                                        },
+                                        x: {
+                                            ticks: {
+                                                font: {
+                                                    size: 24,
+                                                    family: 'Nunito',
+                                                },
+                                                color: 'black'
+                                            },
+                                        },
+                                    },
+                                    color: 'black',
                                 }
-                                });
-                            
-                        });
-                        
-                        
+                            });
+                        }
                     </script>
                 </div>
             </div>
         </div>
     </div>
- 
- 
- </body>
- 
+
+
+</body>
